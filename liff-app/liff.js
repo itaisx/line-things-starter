d6fcdf7a78e12ea144465dc2461cbab0989533df
JLWu817 <46471932+JLWu817@users.noreply.github.com>
2020-01-29 15:59:02 +0700

    Update liff.js

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -199,6 +199,8 @@ function liffConnectToDevice(device) {
         // Begin accessing JSON data here
         var data = JSON.parse(this.response);
         if (request.status >= 200 && request.status < 400) {
+          document.getElementById("residential-name").innerText =
+            data.toString();
           console.log(data);
           console.log(data.residentialName);
           document.getElementById("residential-name").innerText =
