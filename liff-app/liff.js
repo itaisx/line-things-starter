f71d4409ba0f687a2f614ba51f7397a6eae984e5
JLWu817 <46471932+JLWu817@users.noreply.github.com>
2020-03-19 13:33:22 +0700

    Add files via upload

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -73,7 +73,7 @@ function uiToggleDeviceConnected(connected) {
       // Begin accessing JSON data here
       var data = JSON.parse(this.response);
       if (request.status >= 200 && request.status < 400) {
-        if (data.length > 0 && data[0].residentialId == residentialId) {
+        if (data.length > 0 && data[0].residentialId == residentialId && data[0].status == 1) {
           // Hide loading animation
           uiToggleLoadingAnimation(false);
           // Show status connected
